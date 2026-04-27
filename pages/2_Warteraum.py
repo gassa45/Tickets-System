dd2154caecf4fb260770ba4e9c6175f4706607b9
Gassa <gassa45@yahoo.com>
2026-04-27 09:02:11 +0200

    Logout Button von kund und wart entfernt

--- pages/2_Warteraum.py
+++ pages/2_Warteraum.py
@@ -5,15 +5,6 @@ from PIL import Image
 import os
 from languages import translations
 
-################
-####Logout Button
-################
-
-with st.sidebar:
-    if st.session_state.get("logged_in", False):
-        if st.button("Logout"):
-            st.session_state["logged_in"] = False
-            st.rerun()
 
 # ---------------------------------------------------------
 # AUTOMATISCHER BROWSER-MÜLL-SCHUTZ
